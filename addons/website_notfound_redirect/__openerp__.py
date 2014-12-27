21600606079c73b16368a1c331d3f58fd67f142b
Rui Andrada <shingonoide@gmail.com>
2014-12-27 05:19:05 -0200

    First version notfound_redirect

---
+++ addons/website_notfound_redirect/__openerp__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Website NotFound Redirect',
+    'version': '1.0',
+    'category': 'Website',
+    'description': """This module handle the 404 code and redirect to old content if exists.""",
+    'author': 'BarraDev Consulting',
+    'website': 'www.barradev.com',
+    'summary': 'Redirect 404 NotFound content to old url if content exists.',
+    'license': 'AGPL-3',
+    'depends': ['website'],
+    'data': [],
+    'demo': [],
+    'installable': True,
+    'auto_install': True,
+}
+
---
+++ addons/website_notfound_redirect/ir_http.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+import logging
+import urllib2
+
+from openerp.http import request
+from openerp.osv import orm
+
+logger = logging.getLogger(__name__)
+
+class ir_http(orm.AbstractModel):
+    _inherit = 'ir.http'
+
+    def _handle_exception(self, exception, code=500):
+        code = getattr(exception, 'code', code)
+        if code == 404:
+            page = request.httprequest.path
+            logger.warning("404 code... %s" % (page))
+            url = request.registry['ir.config_parameter'].get_param(request.cr,
+                request.uid, 'website.notfound_redirect_url')
+            if url:
+                url_request = "%s%s" % (url, page)
+                logger.info("The redirect url: %s" % (url_request))
+                try:
+                    req = urllib2.Request(url_request)
+                    request_old = urllib2.urlopen(req)
+                except (urllib2.HTTPError, urllib2.URLError):
+                    request_old = False
+            else:
+                request_old = False
+
+            if not request_old:        
+                logger.warning("URL not found: %s" % (url_request))
+                return super(ir_http, self)._handle_exception(exception, code)
+            else:
+                logger.warning("Redirect to %s" % (url_request))
+                return request.redirect(url_request, code=302)
+        
+        return super(ir_http, self)._handle_exception(exception, code)
