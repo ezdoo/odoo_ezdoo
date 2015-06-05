0677a96e20cf03653be9421b1dd32ce80b12f195
Rui Andrada <shingonoide@gmail.com>
2015-06-05 20:49:51 -0300

    Better code formating

--- addons/website_maintenance/models/res_config.py
+++ addons/website_maintenance/models/res_config.py
@@ -3,23 +3,23 @@ from openerp import models, fields, api
 
 
 class website_maintenance_config_settings(models.Model):
-    _inherit= 'website.config.settings'
+    _inherit = 'website.config.settings'
 
     is_on = ['yes', 'true', '1', 'on']
 
     maintenance_mode = fields.Boolean('Enable Maintenance Mode')
     maintenance_message = fields.Char('Custom Maintenance Message')
-    
+
     @api.cr_uid_ids_context
     def set_maintenance_mode(self, cr, uid, ids, context=None):
         config_param = self.pool.get('ir.config_parameter')
         config = self.browse(cr, uid, ids[0], context=context)
         if config.maintenance_mode:
             config_param.set_param(cr, uid,
-                    'website.maintenance_mode', 'yes')
+                                   'website.maintenance_mode', 'yes')
         else:
             config_param.set_param(cr, uid,
-                    'website.maintenance_mode', 'no')
+                                   'website.maintenance_mode', 'no')
 
     @api.cr_uid_ids_context
     def set_maintenance_message(self, cr, uid, ids, context=None):
@@ -29,14 +29,15 @@ class website_maintenance_config_settings(models.Model):
         print type(maintenance_message)
         if maintenance_message not in ['', False]:
             config_param.set_param(cr, uid,
-                'website.maintenance_message', maintenance_message)
+                                   'website.maintenance_message',
+                                   maintenance_message)
 
     @api.cr_uid_ids_context
     def get_default_maintenance_mode(self, cr, uid, ids, fields, context=None):
         config_param = self.pool.get('ir.config_parameter')
         maintenance_param = config_param.get_param(cr, uid,
-                    'website.maintenance_mode')
-        
+                                                   'website.maintenance_mode')
+
         if maintenance_param in self.is_on:
             maintenance_mode = True
         else:
@@ -45,9 +46,10 @@ class website_maintenance_config_settings(models.Model):
         return {'maintenance_mode': maintenance_mode}
 
     @api.cr_uid_ids_context
-    def get_default_maintenance_message(self, cr, uid, ids, fields, context=None):
+    def get_default_maintenance_message(self, cr, uid, ids, fields,
+                                        context=None):
         config_param = self.pool.get('ir.config_parameter')
-        maintenance_message = config_param.get_param(cr, uid,
-                    'website.maintenance_message', '')
+        maintenance_message = \
+            config_param.get_param(cr, uid, 'website.maintenance_message', '')
 
         return {'maintenance_message': maintenance_message}
